29dec36aa4e8fc41f0f976a1aee3eb803529d728
drachu <95628848+drachu@users.noreply.github.com>
2022-04-19 19:35:25 +0200

    changes for form mostly

--- website/models.py
+++ website/models.py
@@ -8,13 +8,13 @@ class Poll(db.Model):
     praca = db.Column(db.String(length=10), nullable=False) #tak-nie
 
     # Czas gry //mieszane//
-    # gra_w_ciagu_12 = db.Column(db.String(length=10), nullable=False) #tak-nie
+    gra_w_ciagu_12 = db.Column(db.String(length=10), nullable=False) #tak-nie
     gra_prof = db.Column(db.String(length=10), nullable=False) #tak-nie
-    # gra_tydz = db.Column(db.Integer(), nullable=False) #lista
-    # gra_tydz_weekend = db.Column(db.Integer(), nullable=False) #suwak
+    gra_tydz = db.Column(db.Integer(), nullable=False) #lista
+    gra_tydz_weekend = db.Column(db.Integer(), nullable=False) #suwak
 
     #Rodzaj gier //mieszane//
-    # gra_typ = db.Column(db.String(length=30), nullable=False) #lista
+    gra_typ = db.Column(db.String(length=30), nullable=False) #lista
     gra_ulubiona = db.Column(db.String(length=30), nullable=False) #textbox
     gra_platforma = db.Column(db.String(length=30), nullable=False) #lista
     gra_klan = db.Column(db.String(length=10), nullable=False) #tak-nie
@@ -27,7 +27,7 @@ class Poll(db.Model):
 
     #Pozytywne odczucia //mieszane//
     pozytyw_ucieczka = db.Column(db.String(length=10), nullable=False) #tak-nie
-    #pozytyw_emocje = db.Column(db.String(length=30), nullable=False) #szczescie, gniew, smutek, strach
+    pozytyw_emocje = db.Column(db.String(length=30), nullable=False) #szczescie, gniew, smutek, strach
     pozytyw_samokontrola = db.Column(db.String(length=10), nullable=False) #tak-nie
     pozytyw_koncentracja = db.Column(db.Integer(), nullable=False) #1-5
     pozytyw_koordynacja = db.Column(db.Integer(), nullable=False) #1-5
@@ -40,7 +40,6 @@ class Poll(db.Model):
     negatyw_gniew = db.Column(db.String(length=30), nullable=False)
     
     #Stosunki z innymi, deprecha //nigdy, rzadzko, czesto, zawsze//
-    
     tow_kontakty = db.Column(db.Integer(), nullable=False) #1-5
     tow_inni_sie_przejmuja = db.Column(db.String(length=30), nullable=False)
     tow_brak_towarzystwa = db.Column(db.String(length=30), nullable=False)
--- website/routes.py
+++ website/routes.py
@@ -1,5 +1,6 @@
+from unicodedata import name
 from run import app
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, request, url_for, flash
 from website.forms import pytanieForm
 from website.models import Poll
 from website import db
@@ -9,60 +10,21 @@ from website import db
 def home_page():
     return render_template('home.html')
 
-@app.route('/form', methods=['GET', 'POST'])
+@app.route('/form',methods=['GET', 'POST'])
 def form_page():
     form = pytanieForm()
-    if form.validate_on_submit():
-        
-        Poll_to_add = Poll(
-            rok=form.rok.data,
-            zwiazek=form.zwiazek.data,
-            praca=form.praca.data,
-            gra_prof=form.gra_prof.data,
-            gra_ulubiona=form.gra_ulubiona.data,
-            gra_platforma=form.gra_platforma.data,
-            gra_klan=form.gra_klan.data,
-            gra_grind=form.gra_grind.data,
-            pandemia_start=form.pandemia_start.data,
-            pandemia_gra_wiecej=form.pandemia_gra_wiecej.data,
-            pandemia_czas=form.pandemia_czas.data,
-            pozytyw_ucieczka=form.pozytyw_ucieczka.data,
-            pozytyw_samokontrola=form.pozytyw_samokontrola.data,
-            pozytyw_koncentracja=form.pozytyw_koncentracja.data,
-            pozytyw_koordynacja=form.pozytyw_koordynacja.data,
-            negatyw_gra_pomimo_konsekwencji=form.negatyw_gra_pomimo_konsekwencji.data,
-            negatyw_gra_dluzej=form.negatyw_gra_dluzej.data,
-            negatyw_gra_nad_inne_aktywnosci=form.negatyw_gra_nad_inne_aktywnosci.data,
-            negatyw_zaniedbania=form.negatyw_zaniedbania.data,
-            negatyw_gniew=form.negatyw_gniew.data,
-            tow_kontakty=form.tow_kontakty.data,
-            tow_inni_sie_przejmuja=form.tow_inni_sie_przejmuja.data,
-            tow_brak_towarzystwa=form.tow_brak_towarzystwa.data,
-            tow_izolacja=form.tow_izolacja.data,
-            tow_przytloczenie=form.tow_przytloczenie.data)
-
-        db.session.add(Poll_to_add)
-        db.session.commit()
-
-        flash(f"Zapisano odpowiedzi.", category='success')
-        return redirect(url_for('thanks_page'))
-    else:
-        flash(f'Proszę uzupełnić brakujące dane.', category='danger')
-    if form.errors != {}: #If there are not errors from the validations
-        for err_msg in form.errors.values():
-            flash(f'Proszę uzupełnić brakujące dane: {err_msg}', category='danger')
-    
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user_poll = Poll(rok=form.rok.data, zwiazek=form.zwiazek.data, praca=form.praca.data, gra_w_ciagu_12=form.gra_w_ciagu_12.data, gra_prof=form.gra_prof.data, gra_tydz=form.gra_tydz.data, gra_tydz_weekend=form.gra_tydz_weekend.data,gra_typ=form.gra_typ.data ,gra_ulubiona=form.gra_ulubiona.data, gra_platforma=form.gra_platforma.data, gra_klan=form.gra_klan.data, gra_grind=form.gra_grind.data, pandemia_start=form.pandemia_start.data, pandemia_gra_wiecej=form.pandemia_gra_wiecej.data, pandemia_czas=form.pandemia_czas.data, pozytyw_ucieczka=form.pozytyw_ucieczka.data, pozytyw_emocje=form.pozytyw_emocje.data, pozytyw_samokontrola=form.pozytyw_samokontrola.data, pozytyw_koncentracja=form.pozytyw_koncentracja.data, pozytyw_koordynacja=form.pozytyw_koordynacja.data, negatyw_gra_pomimo_konsekwencji=form.negatyw_gra_pomimo_konsekwencji.data, negatyw_gra_dluzej=form.negatyw_gra_dluzej.data, negatyw_gra_nad_inne_aktywnosci=form.negatyw_gra_nad_inne_aktywnosci.data, negatyw_zaniedbania=form.negatyw_zaniedbania.data, negatyw_gniew=form.negatyw_gniew.data, tow_kontakty=form.tow_kontakty.data, tow_inni_sie_przejmuja=form.tow_inni_sie_przejmuja.data, tow_brak_towarzystwa=form.tow_brak_towarzystwa.data, tow_izolacja=form.tow_izolacja.data, tow_przytloczenie=form.tow_przytloczenie.data)
+            db.session.add(user_poll)
+            db.session.commit()
+       
+            return redirect(url_for('thanks_page'))
+        else:
+            flash('Uzupełnij odpowiedź!', category='error')
     return render_template('form.html', form=form)
 
 
 @app.route('/thanks')
 def thanks_page():
     return render_template('thanks.html')
-
-
-@app.route('/test',methods=['GET', 'POST'])
-def test_page():
-    form = pytanieForm()
-    if form.validate_on_submit():
-        pass
-    return render_template('test.html', form=form)

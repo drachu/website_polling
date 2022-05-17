f729a20fecd7271c2a1dfcd23405834b0be314a2
Jakub Konert <83219666+JakubKonert@users.noreply.github.com>
2022-05-17 12:39:13 +0200

    bez auto w drzewie decyzyjnym

--- Analiza_wynikow/Eksploracja.py
+++ Analiza_wynikow/Eksploracja.py
@@ -32,49 +32,33 @@ def run():
     #database = MF.EncodeLabel(database, labelsToEncode, LabelEnc)
 
     class_names=['Nie_Uzal', 'Tak_Uzal']
-    # feature_names = ['gra_prof', 'gra_tydz_weekend', 'gra_typ', 'gra_platforma', 'gra_klan',
-    #                  'gra_grind', 'pandemia_start', 'pandemia_gra_wiecej', 'pandemia_czas',
-    #                  'pozytyw_ucieczka', 'pozytyw_samokontrola', 'pozytyw_koncentracja',
-    #                  'pozytyw_koordynacja', 'negatyw_gra_pomimo_konsekwencji',
-    #                  'negatyw_gra_dluzej', 'negatyw_gra_nad_inne_aktywnosci',
-    #                  'negatyw_zaniedbania', 'negatyw_gniew', 'tow_kontakty',
-    #                  'tow_inni_sie_przejmuja', 'tow_brak_towarzystwa', 'tow_izolacja',
-    #                  'tow_przytloczenie', 'rok', 'zwiazek', 'praca']
+    feature_names = ['gra_prof', 'gra_tydz_weekend', 'gra_typ', 'gra_platforma', 'gra_klan',
+                     'gra_grind', 'pandemia_start', 'pandemia_gra_wiecej', 'pandemia_czas',
+                     'pozytyw_ucieczka', 'pozytyw_samokontrola', 'pozytyw_koncentracja',
+                     'pozytyw_koordynacja', 'negatyw_gra_pomimo_konsekwencji',
+                     'negatyw_gra_dluzej', 'negatyw_gra_nad_inne_aktywnosci',
+                     'negatyw_zaniedbania', 'negatyw_gniew', 'tow_kontakty',
+                     'tow_inni_sie_przejmuja', 'tow_brak_towarzystwa', 'tow_izolacja',
+                     'tow_przytloczenie', 'rok', 'zwiazek', 'praca']
 
     """Data Mining"""
     # MF.skupieniaLokiec(database)
     # MF.analizaSkupien(database,5)
 
-    # MF.DecisionTree(database,feature_names,class_names,image_name='Gini_10_0_None', criterion='gini', max_deph=10,random_state=0,max_features=None)
-    # MF.DecisionTree(database,feature_names,class_names, image_name='Gini_10_1_None',criterion='gini', max_deph=10,random_state=1,max_features=None)
-    # MF.DecisionTree(database,feature_names,class_names,image_name='Entropy_10_0_None', criterion='entropy', max_deph=10,random_state=0,max_features=None)
-    # MF.DecisionTree(database,feature_names,class_names, image_name='Entropy_10_1_None',criterion='entropy', max_deph=10,random_state=1,max_features=None)
+    MF.DecisionTree(database,feature_names,class_names,image_name='Gini_10_0_None', criterion='gini', max_deph=10,random_state=0)
+    MF.DecisionTree(database,feature_names,class_names, image_name='Gini_10_1_None',criterion='gini', max_deph=10,random_state=1)
+    MF.DecisionTree(database,feature_names,class_names,image_name='Entropy_10_0_None', criterion='entropy', max_deph=10,random_state=0)
+    MF.DecisionTree(database,feature_names,class_names, image_name='Entropy_10_1_None',criterion='entropy', max_deph=10,random_state=1 )
 
-    # MF.DecisionTree(database,feature_names,class_names, image_name='Gini_4_0_None',criterion='gini', max_deph=4,random_state=0,max_features=None)
-    # MF.DecisionTree(database,feature_names,class_names,image_name='Gini_4_1_None' ,criterion='gini', max_deph=4,random_state=1,max_features=None)
-    # MF.DecisionTree(database,feature_names,class_names,image_name='Entropy_4_0_None', criterion='entropy', max_deph=4,random_state=0,max_features=None)
-    # MF.DecisionTree(database,feature_names,class_names,image_name='Entropy_4_1_None', criterion='entropy', max_deph=4,random_state=1,max_features=None)
+    MF.DecisionTree(database,feature_names,class_names, image_name='Gini_4_0_None',criterion='gini', max_deph=4,random_state=0 )
+    MF.DecisionTree(database,feature_names,class_names,image_name='Gini_4_1_None' ,criterion='gini', max_deph=4,random_state=1 )
+    MF.DecisionTree(database,feature_names,class_names,image_name='Entropy_4_0_None', criterion='entropy', max_deph=4,random_state=0 )
+    MF.DecisionTree(database,feature_names,class_names,image_name='Entropy_4_1_None', criterion='entropy', max_deph=4,random_state=1 )
 
-    # MF.DecisionTree(database,feature_names,class_names, image_name='Gini_6_0_None',criterion='gini', max_deph=6,random_state=0,max_features=None)
-    # MF.DecisionTree(database,feature_names,class_names,image_name='Gini_6_1_None', criterion='gini', max_deph=6,random_state=1,max_features=None)
-    # MF.DecisionTree(database,feature_names,class_names,image_name='Entropy_6_0_None', criterion='entropy', max_deph=6,random_state=0,max_features=None)
-    # MF.DecisionTree(database,feature_names,class_names,image_name='Entropy_6_1_None', criterion='entropy', max_deph=6,random_state=1,max_features=None)
-
-
-    # MF.DecisionTree(database,feature_names,class_names,image_name='Gini_10_0_auto', criterion='gini', max_deph=10,random_state=0,max_features='auto')
-    # MF.DecisionTree(database,feature_names,class_names, image_name='Gini_10_1_auto',criterion='gini', max_deph=10,random_state=1,max_features='auto')
-    # MF.DecisionTree(database,feature_names,class_names,image_name='Entropy_10_0_auto', criterion='entropy', max_deph=10,random_state=0,max_features='auto')
-    # MF.DecisionTree(database,feature_names,class_names, image_name='Entropy_10_1_auto',criterion='entropy', max_deph=10,random_state=1,max_features='auto')
-
-    # MF.DecisionTree(database,feature_names,class_names, image_name='Gini_4_0_auto',criterion='gini', max_deph=4,random_state=0,max_features='auto')
-    # MF.DecisionTree(database,feature_names,class_names,image_name='Gini_4_1_auto' ,criterion='gini', max_deph=4,random_state=1,max_features='auto')
-    # MF.DecisionTree(database,feature_names,class_names,image_name='Entropy_4_0_auto', criterion='entropy', max_deph=4,random_state=0,max_features='auto')
-    # MF.DecisionTree(database,feature_names,class_names,image_name='Entropy_4_1_auto', criterion='entropy', max_deph=4,random_state=1,max_features='auto')
-
-    # MF.DecisionTree(database,feature_names,class_names, image_name='Gini_6_0_auto',criterion='gini', max_deph=6,random_state=0,max_features='auto')
-    # MF.DecisionTree(database,feature_names,class_names,image_name='Gini_6_1_auto', criterion='gini', max_deph=6,random_state=1,max_features='auto')
-    # MF.DecisionTree(database,feature_names,class_names,image_name='Entropy_6_0_auto', criterion='entropy', max_deph=6,random_state=0,max_features='auto')
-    # MF.DecisionTree(database,feature_names,class_names,image_name='Entropy_6_1_auto', criterion='entropy', max_deph=6,random_state=1,max_features='auto')
+    MF.DecisionTree(database,feature_names,class_names, image_name='Gini_6_0_None',criterion='gini', max_deph=6,random_state=0 )
+    MF.DecisionTree(database,feature_names,class_names,image_name='Gini_6_1_None', criterion='gini', max_deph=6,random_state=1 )
+    MF.DecisionTree(database,feature_names,class_names,image_name='Entropy_6_0_None', criterion='entropy', max_deph=6,random_state=0 )
+    MF.DecisionTree(database,feature_names,class_names,image_name='Entropy_6_1_None', criterion='entropy', max_deph=6,random_state=1 )
 
     #6 najwazniejszych cech
     columnsToDrop = ['gra_prof','gra_typ','gra_platforma','gra_grind','pandemia_start','pandemia_gra_wiecej','pandemia_czas','pozytyw_ucieczka','pozytyw_samokontrola','pozytyw_koncentracja','pozytyw_koordynacja','negatyw_gra_pomimo_konsekwencji','negatyw_gra_dluzej','tow_kontakty','tow_inni_sie_przejmuja','tow_brak_towarzystwa','tow_izolacja','rok','zwiazek','praca']
@@ -83,36 +67,20 @@ def run():
     # MF.skupieniaLokiec(database)
     # MF.analizaSkupien(database,5)
 
-    MF.DecisionTree(database,feature_names,class_names,image_name='6_Gini_10_0_None', criterion='gini', max_deph=10,random_state=0,max_features=None)
-    MF.DecisionTree(database,feature_names,class_names, image_name='6_Gini_10_1_None',criterion='gini', max_deph=10,random_state=1,max_features=None)
-    MF.DecisionTree(database,feature_names,class_names,image_name='6_Entropy_10_0_None', criterion='entropy', max_deph=10,random_state=0,max_features=None)
-    MF.DecisionTree(database,feature_names,class_names, image_name='6_Entropy_10_1_None',criterion='entropy', max_deph=10,random_state=1,max_features=None)
+    MF.DecisionTree(database,feature_names,class_names,image_name='6_Gini_10_0_None', criterion='gini', max_deph=10,random_state=0 )
+    MF.DecisionTree(database,feature_names,class_names, image_name='6_Gini_10_1_None',criterion='gini', max_deph=10,random_state=1 )
+    MF.DecisionTree(database,feature_names,class_names,image_name='6_Entropy_10_0_None', criterion='entropy', max_deph=10,random_state=0 )
+    MF.DecisionTree(database,feature_names,class_names, image_name='6_Entropy_10_1_None',criterion='entropy', max_deph=10,random_state=1 )
 
-    MF.DecisionTree(database,feature_names,class_names, image_name='6_Gini_4_0_None',criterion='gini', max_deph=4,random_state=0,max_features=None)
-    MF.DecisionTree(database,feature_names,class_names,image_name='6_Gini_4_1_None' ,criterion='gini', max_deph=4,random_state=1,max_features=None)
-    MF.DecisionTree(database,feature_names,class_names,image_name='6_Entropy_4_0_None', criterion='entropy', max_deph=4,random_state=0,max_features=None)
-    MF.DecisionTree(database,feature_names,class_names,image_name='6_Entropy_4_1_None', criterion='entropy', max_deph=4,random_state=1,max_features=None)
+    MF.DecisionTree(database,feature_names,class_names, image_name='6_Gini_4_0_None',criterion='gini', max_deph=4,random_state=0 )
+    MF.DecisionTree(database,feature_names,class_names,image_name='6_Gini_4_1_None' ,criterion='gini', max_deph=4,random_state=1 )
+    MF.DecisionTree(database,feature_names,class_names,image_name='6_Entropy_4_0_None', criterion='entropy', max_deph=4,random_state=0 )
+    MF.DecisionTree(database,feature_names,class_names,image_name='6_Entropy_4_1_None', criterion='entropy', max_deph=4,random_state=1 )
 
-    MF.DecisionTree(database,feature_names,class_names, image_name='6_Gini_6_0_None',criterion='gini', max_deph=6,random_state=0,max_features=None)
-    MF.DecisionTree(database,feature_names,class_names,image_name='6_Gini_6_1_None', criterion='gini', max_deph=6,random_state=1,max_features=None)
-    MF.DecisionTree(database,feature_names,class_names,image_name='6_Entropy_6_0_None', criterion='entropy', max_deph=6,random_state=0,max_features=None)
-    MF.DecisionTree(database,feature_names,class_names,image_name='6_Entropy_6_1_None', criterion='entropy', max_deph=6,random_state=1,max_features=None)
-
-
-    MF.DecisionTree(database,feature_names,class_names,image_name='6_Gini_10_0_auto', criterion='gini', max_deph=10,random_state=0,max_features='auto')
-    MF.DecisionTree(database,feature_names,class_names, image_name='6_Gini_10_1_auto',criterion='gini', max_deph=10,random_state=1,max_features='auto')
-    MF.DecisionTree(database,feature_names,class_names,image_name='6_Entropy_10_0_auto', criterion='entropy', max_deph=10,random_state=0,max_features='auto')
-    MF.DecisionTree(database,feature_names,class_names, image_name='6_Entropy_10_1_auto',criterion='entropy', max_deph=10,random_state=1,max_features='auto')
-
-    MF.DecisionTree(database,feature_names,class_names, image_name='6_Gini_4_0_auto',criterion='gini', max_deph=4,random_state=0,max_features='auto')
-    MF.DecisionTree(database,feature_names,class_names,image_name='6_Gini_4_1_auto' ,criterion='gini', max_deph=4,random_state=1,max_features='auto')
-    MF.DecisionTree(database,feature_names,class_names,image_name='6_Entropy_4_0_auto', criterion='entropy', max_deph=4,random_state=0,max_features='auto')
-    MF.DecisionTree(database,feature_names,class_names,image_name='6_Entropy_4_1_auto', criterion='entropy', max_deph=4,random_state=1,max_features='auto')
-
-    MF.DecisionTree(database,feature_names,class_names, image_name='6_Gini_6_0_auto',criterion='gini', max_deph=6,random_state=0,max_features='auto')
-    MF.DecisionTree(database,feature_names,class_names,image_name='6_Gini_6_1_auto', criterion='gini', max_deph=6,random_state=1,max_features='auto')
-    MF.DecisionTree(database,feature_names,class_names,image_name='6_Entropy_6_0_auto', criterion='entropy', max_deph=6,random_state=0,max_features='auto')
-    MF.DecisionTree(database,feature_names,class_names,image_name='6_Entropy_6_1_auto', criterion='entropy', max_deph=6,random_state=1,max_features='auto')
+    MF.DecisionTree(database,feature_names,class_names, image_name='6_Gini_6_0_None',criterion='gini', max_deph=6,random_state=0 )
+    MF.DecisionTree(database,feature_names,class_names,image_name='6_Gini_6_1_None', criterion='gini', max_deph=6,random_state=1 )
+    MF.DecisionTree(database,feature_names,class_names,image_name='6_Entropy_6_0_None', criterion='entropy', max_deph=6,random_state=0 )
+    MF.DecisionTree(database,feature_names,class_names,image_name='6_Entropy_6_1_None', criterion='entropy', max_deph=6,random_state=1 )
 
     # MF.sprawdzenieIlosciIstnonychCech(database,6)
     # MF.istotnoscCechLas(database,0.06)
